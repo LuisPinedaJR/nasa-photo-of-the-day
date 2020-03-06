0a1e46ad932ff17d95d70599182d28ead0452389
Luis Pineda <lspnd00@gmail.com>
2020-03-05 19:55:55 -0500

    added spread to data for second component

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from 'react'
 
-// const CardComponent = props => {
-//   return (
-//     <div className="card-div">
-//       <p className="card-p">{props.explanation}</p>
-//     </div>
-//   )
-// }
+const CardComponent = props => {
+  return (
+    <div className="card-div">
+      <p className="card-p">{props.explanation}</p>
+    </div>
+  )
+}
 
-// export default CardComponent
+export default CardComponent
---
+++ src/components/test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+console.log('yes', yes)
+
+console.log('okkk', ok)
+
+console.log('yessir')
+
+console.log('yup')
+
+console.log({ var })
